e80cb4f3242400abf5ee063bc95196929142a1fa
DDmsngr <khti@bk.ru>
2026-05-28 13:21:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
 export default defineConfig({
   plugins: [react()],
   // Имя репозитория на GitHub — именно оно идёт в base
